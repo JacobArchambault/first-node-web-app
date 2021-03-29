44bf3299973869e1f38704deff78f54a7b98e483
Jacob Archambault <jacob.archambault@waystar.com>
2021-03-28 23:52:50 -0400

    for app.get, skip string building and send it directly

--- index.js
+++ index.js
@@ -3,23 +3,13 @@ var app = express();
 var bodyParser = require('body-parser');      // need to have installed body-parser with npm
 app.use(bodyParser.urlencoded({ extended: true }));     // allows handling complex objects
 
-app.get('/', function (req, res) {             // send the default page
-    var response = "<html><body><p><a href='/location?city=Louisville&state=KY'>city & state</a>";
-    response += "<p> <form action='/handleForm' method='post'>";
-    response += "Name: <input name='username'> <p> My Favorite Seasons: <br> ";
-    response += "<input type=checkbox name='season' value='spring'>Spring <br> ";
-    response += "<input type=checkbox name='season' value='summer'>Summer<br>";
-    response += "<input type=checkbox name='season' value='fall'>Fall<br>";
-    response += "<input type=checkbox name='season' value='winter'>Winter<br> <p>";
-    response += "<input type=submit value='Submit form!'></form></body> </html>";
-    res.send(response);
-});
+app.get('/', (_req, res) => res.send("<html><body><p><a href='/location?city=Louisville&state=KY'>city & state</a><p> <form action='/handleForm' method='post'>Name: <input name='username'> <p> My Favorite Seasons: <br> <input type=checkbox name='season' value='spring'>Spring <br> <input type=checkbox name='season' value='summer'>Summer<br><input type=checkbox name='season' value='fall'>Fall<br><input type=checkbox name='season' value='winter'>Winter<br> <p><input type=submit value='Submit form!'></form></body> </html>"));
 
 app.get('/location', (req, res) => {
-        console.log(req.query); // displays query values as an object
-        console.log(`number of values: ${Object.keys(req.query).length}`);
-        res.send(`Location: ${req.query.city}, ${req.query.state}`);
-    });
+    console.log(req.query); // displays query values as an object
+    console.log(`number of values: ${Object.keys(req.query).length}`);
+    res.send(`Location: ${req.query.city}, ${req.query.state}`);
+});
 
 app.post('/handleForm', (req, res) => res.send(req.body.username + " chose: " + req.body.season));
 
